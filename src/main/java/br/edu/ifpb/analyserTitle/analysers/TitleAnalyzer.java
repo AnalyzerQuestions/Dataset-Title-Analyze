9b9c0062f6bf0a8632d4479d8c95edfe73a4d1df
Vinicius <vinicius.gouveia.gomes@gmail.com>
2017-10-02 01:41:09 -0300

    Analizers methods

--- src/main/java/br/edu/ifpb/analyserTitle/analysers/TitleAnalyzer.java
+++ src/main/java/br/edu/ifpb/analyserTitle/analysers/TitleAnalyzer.java
@@ -1,5 +1,7 @@
 package br.edu.ifpb.analyserTitle.analysers;
 
+import br.edu.ifpb.analyserTitle.util.StringTokenizerUtils;
+import br.edu.ifpb.analyserTitle.util.StringUtil;
 
 /**
  * 
@@ -14,5 +16,69 @@ package br.edu.ifpb.analyserTitle.analysers;
  * @author <a href="https://github.com/JoseRafael97">Rafael Feitosa</a>
  */
 public class TitleAnalyzer {
+	
+	public Integer isMediumSizeTitle(String title) {
+		String str = StringUtil.removeConnective(title);
+		str = StringUtil.removeCharacterSpecial(str);
+		String[] strPart = StringTokenizerUtils.parseToken(str);
 
+		if (strPart.length > 2 && strPart.length <= 8)
+			return 1;
+		else
+			return 0;
+	}
+	
+	public Integer isSmallSizeTitle(String title) {
+		String str = StringUtil.removeConnective(title);
+		str = StringUtil.removeCharacterSpecial(str);
+		String[] strPart = StringTokenizerUtils.parseToken(str);
+
+		if (strPart.length <= 3)
+			return 1;
+		else
+			return 0;
+	}
+	
+	public Integer isTotallyUpperCase(String title){
+		
+		boolean result = title.equals(title.toUpperCase());
+		
+		if(result == true){
+			return 1;
+		}
+		
+		return 0;
+	}
+	
+	public Integer isPartiallyUpperCase(String title){
+		
+		String str = StringUtil.removeConnective(title);
+		str = StringUtil.removeCharacterSpecial(str);
+		String[] strPart = StringTokenizerUtils.parseToken(str);
+		
+		int cont = 0;
+		
+		for(String s: strPart){
+			if(s.equals(s.toUpperCase())){
+				cont++;
+			}
+		}
+		
+		if(cont >= 2){
+			return 1;
+		}
+		
+		return 0;
+	}
+
+	public Integer containsHelpOrUrgent(String title){
+		
+		boolean result = title.contains("ajuda") || title.contains("urgente");
+		
+		if(result == true){
+			return 1;
+		}
+		
+		return 0;
+	}
 }
